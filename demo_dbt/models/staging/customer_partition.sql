f9bdf6a1626ecf76386837e1b6bf95b26d1f7d09
vikasvk05 <vikas@DESKTOP-BOGVGP7.localdomain>
2023-07-06 11:44:32 +0530

    created first dbt working model

---
+++ demo_dbt/models/staging/customer_partition.sql
@@ -0,0 +1,22 @@
+
+
+{{ config(
+    materialized="incremental",
+    partition_by={
+      "field": "dt",
+      "data_type": "date"
+    }
+) }}
+
+-- SELECT * FROM `learning-gcp-self-test.Leaning_bq.us_emp_data`
+
+
+-- Query using jinja template --
+
+with source as (
+
+    select * from {{ source('learning', 'customer1') }}
+)
+select 
+    *
+from source --where dt='2023-04-22'
